6956374da5ad1db1af2ca4c3965ffc37cbb8aba3
Adam Diab <76654485+diab88@users.noreply.github.com>
2024-01-26 19:06:40 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,7 @@ const Home = () => (
 
  <div>
 
-<<<<<<< Updated upstream
 <h1>Hello world from Berlin again , it's from Berlin :)</h1>
-=======
-<<<<<<< HEAD
-<h1>Hello world, Adam Diab:)</h1>
-=======
-<h1>Hello world from Berlin again , it's from Berlin :)</h1>
->>>>>>> e14b78f09e83de4442ca4f53041172d2f217b5bf
->>>>>>> Stashed changes
 
  </div>
 );
