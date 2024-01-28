ab2faea3a55379d5450a6e6f6e9b04937c357200
Adam Diab <76654485+diab88@users.noreply.github.com>
2024-01-28 17:38:45 +0100

    new changes!

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,11 @@ const Home = () => (
 
  <div>
 
+<<<<<<< Updated upstream
 <h1>Hello world from Berlin again, coming soon to Thai!!!</h1>
+=======
+<h1>Hello world from Berlin again!!!</h1>
+>>>>>>> Stashed changes
 
  </div>
 );
